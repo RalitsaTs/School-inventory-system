cd9696728a30ba68db07c5302646eadc3397ce4d
Zdravko Kolev <zdrawko.kolew@gmail.com>
2025-10-24 23:37:18 +0300

    School Inventory System updates

--- src/app/models/generate-alist-of-equipment-requests-including-it/equipment-requests-type.ts
+++
@@ -1,7 +0,0 @@
-export interface EquipmentRequestsType {
-  item_name: string;
-  requester_name: string;
-  request_date: Date;
-  status: string;
-  notes: string;
-}
--- src/app/models/student-borrowed-items-list/borrowed-items-type.ts
+++
@@ -1,6 +0,0 @@
-export interface BorrowedItemsType {
-  item_name: string;
-  borrow_date: Date;
-  return_date: Date;
-  status: string;
-}

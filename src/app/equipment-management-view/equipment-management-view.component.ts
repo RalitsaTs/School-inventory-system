d27de16fbad68f54b73a2d2852643a40d1d05961
zdrawku <zkolev@infragistics.com>
2025-10-26 19:48:08 +0200

    Fix loading buttons lack

--- src/app/equipment-management-view/equipment-management-view.component.ts
+++ src/app/equipment-management-view/equipment-management-view.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { IGX_NAVBAR_DIRECTIVES, IGX_NAVIGATION_DRAWER_DIRECTIVES, IgxIconButtonDirective, IgxIconComponent, IgxRippleDirective, IgxToggleActionDirective } from 'igniteui-angular';
 import { AuthService } from '../services/auth.service';
 import { CommonModule } from '@angular/common';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-equipment-management-view',
@@ -10,8 +11,10 @@ import { CommonModule } from '@angular/common';
   templateUrl: './equipment-management-view.component.html',
   styleUrls: ['./equipment-management-view.component.scss']
 })
-export class EquipmentManagementViewComponent implements OnInit {
+export class EquipmentManagementViewComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
   public currentUser: any = null;
+  public isAuthLoaded = false;
 
   constructor(
     public authService: AuthService,
@@ -19,9 +22,31 @@ export class EquipmentManagementViewComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.authService.currentUser$.subscribe(user => {
+    // Subscribe to current user changes and mark auth as loaded
+    this.authService.currentUser$.pipe(takeUntil(this.destroy$)).subscribe(user => {
       this.currentUser = user;
+      this.isAuthLoaded = true;
     });
+
+    // If user is already authenticated but currentUser is null, force reload
+    if (this.authService.isAuthenticated() && !this.currentUser) {
+      this.authService.getCurrentUser().pipe(takeUntil(this.destroy$)).subscribe({
+        next: (user) => {
+          this.currentUser = user;
+          this.isAuthLoaded = true;
+        },
+        error: () => {
+          this.isAuthLoaded = true;
+        }
+      });
+    } else {
+      this.isAuthLoaded = true;
+    }
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   public onLogout() {

8434134046067c06c71b3ab0756e2b529e95d10b
zdrawku <zkolev@infragistics.com>
2025-10-26 06:41:40 +0200

    First commit

--- src/app/register-view/register-view.component.ts
+++ src/app/register-view/register-view.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective, IgxRippleDirective } from 'igniteui-angular';
+import { IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective, IgxRippleDirective } from '@infragistics/igniteui-angular';
 
 @Component({
   selector: 'app-register-view',
